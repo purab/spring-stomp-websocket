4196aa9308124158d6a560397442e02e34d2ca80
Purab <purabdk@yahoo.co.in>
2021-10-18 08:50:21 +0530

    first commit

---
+++ spring-stomp-websocket/src/main/java/com/pkharat/app/springstompwebsocket/SpringStompWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.pkharat.app.springstompwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStompWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStompWebsocketApplication.class, args);
+	}
+
+}
---
+++ spring-stomp-websocket/src/test/java/com/pkharat/app/springstompwebsocket/SpringStompWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkharat.app.springstompwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStompWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
